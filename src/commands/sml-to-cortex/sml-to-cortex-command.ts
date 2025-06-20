7fa98b9f1330768f6743d47b7244dd548acbef70
Aaron-Oshiro <aaron.oshiro@atscale.com>
2025-06-20 15:55:59 -0400

    Update sml-to-cortex command, make it similar to dbt-to-sml. Moved parseInput clean file logic to shared file-system-util

--- src/commands/sml-to-cortex/sml-to-cortex-command.ts
+++ src/commands/sml-to-cortex/sml-to-cortex-command.ts
@@ -1,51 +1,65 @@
-import { Args, Flags, Command } from "@oclif/core";
-import path from "path";
+import { Flags, Command } from "@oclif/core";
 import fs from "fs/promises";
 import yaml from "js-yaml";
+import path from "path";
 import { CommandLogger } from "../../shared/command-logger";
 import { Logger } from "../../shared/logger";
 import CortexConverter from "./cortex-converter/cortex-converter";
 import { CortexConverterResult } from "./cortex-models/CortexConverterResult";
-import { ensureDir } from "../../shared/file-system-util";
+import { convertInput, parseInput } from "../../shared/file-system-util";
 
-export class SMLToCortex extends Command {
-  static description = "Convert from SML to Snowflake Cortex Analyst yaml.";
-  static args = {
-    inPath: Args.directory({
-      description: "Path to the SML directory to convert",
-      required: true,
-    }),
-  };
+export class SMLToCortexCommand extends Command {
+  static description = "Convert from SML to Snowflake Cortex Analyst yaml";
 
   static flags = {
-    "output-path": Flags.string({
+    source: Flags.directory({
+      description: "Source folder",
+      default: "./",
+      required: false,
+      aliases: ["s"],
+    }),
+    output: Flags.directory({
       description: "Directory in which to write cortex yaml output file(s)",
       required: false,
-      default: "./cortexOutput/",
+      default: "./cortex_output/",
+      aliases: ["o"],
+    }), 
+    clean: Flags.boolean({
+      description: "Clean the output folder contents without the .git folder",
+      required: false,
+      default: false,
     }),
   };
 
   static examples = [
-    "<%= config.bin %> <%= command.id %> ./sml-input-path",
-    '<%= config.bin %> <%= command.id %> ./sml-input-path --output-path="path-to-cortex-file"',
+    "<%= config.bin %> <%= command.id %> ", 
+    "<%= config.bin %> <%= command.id %> --clean",
+    "<%= config.bin %> <%= command.id %> -source ./sml-source-path -output ./cortex-output-path",
+    "<%= config.bin %> <%= command.id %> -s ./sml-source-path -o ./cortex-output-path",
+    "<%= config.bin %> <%= command.id %> -s ./sml-source-path -o ./cortex-output-path --clean",
   ];
 
   async run() {
-    const { args, flags } = await this.parse(SMLToCortex);
-    await this.convertToCortex(args.inPath, flags["output-path"]);
+    const { flags } = await this.parse(SMLToCortexCommand);
+    await this.convertToCortex({
+      sourcePath: flags.source,
+      outputPath: flags.output,
+      clean: flags.clean,
+  });
   }
 
-  private async convertToCortex(inPath: string, outPath: string) {
-    const resolvedInPath = path.resolve(inPath);
-    let resolvedOutPath = path.resolve(outPath);
-    if (!resolvedOutPath.endsWith("/")) {
-      resolvedOutPath += "/";
-    }
+  private async convertToCortex(input: convertInput) {
 
     const logger = CommandLogger.for(this);
 
+    const { absoluteOutputPath, absoluteSourcePath } = await parseInput(
+      input,
+      logger,
+      this
+    );
+
     const cortexConverter = new CortexConverter(logger);
-    const cortexModels = await cortexConverter.convertYamlFiles(resolvedInPath);
+    const cortexModels = await cortexConverter.convertYamlFiles(absoluteSourcePath);
     const numModels = cortexModels.models.length;
 
     logger.info(
@@ -53,10 +67,12 @@ export class SMLToCortex extends Command {
     );
 
     // Write to files
-    await saveCortexYamlFiles(cortexModels, resolvedOutPath, logger)
-      .then(() => logger.info("Completed writing Cortex yaml files"))
-      .catch(console.error);
-
+    try {
+      await saveCortexYamlFiles(cortexModels, absoluteOutputPath, logger);
+      logger.info("Completed writing Cortex yaml files")
+    } catch (err) {
+      logger.error(`Error writing Cortex yaml file(s): ${err}`);
+    }
     this.exit(0);
   }
 }
@@ -67,18 +83,16 @@ async function saveCortexYamlFiles(
   logger: Logger,
 ) {
   try {
-    await ensureDir(outputDir);
-
     await Promise.all(
       cortexModels.models.map(async (obj) => {
         const yamlStr = yaml.dump(obj);
         const fileName = `${obj.name}.yml`;
-        const filePath = `${outputDir}${fileName}`;
+        const filePath = path.join(outputDir, fileName);
         await fs.writeFile(filePath, yamlStr, "utf8");
         logger.info(`Wrote Cortex yaml file to: ${filePath}`);
       }),
     );
   } catch (err) {
-    console.error("Error saving Cortex yaml file(s):", err);
+    logger.error(`Error saving Cortex yaml file(s): ${err}`);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Command, execute } from "@oclif/core";
 import { DbtToSmlCommand } from "./commands/dbt-to-sml/dbt-to-sml-command";
-import { SMLToCortex } from "./commands/sml-to-cortex/sml-to-cortex-command";
+import { SMLToCortexCommand } from "./commands/sml-to-cortex/sml-to-cortex-command";
 
 export { default as INIT_HOOK } from "./hooks/init/init";
 
@@ -8,7 +8,7 @@ export const COMMANDS: Record<string, Command.Class> = {
   //TO SML COMMANDS
   "dbt-to-sml": DbtToSmlCommand,
   //FROM SML COMMAND
-  "sml-to-cortex": SMLToCortex
+  "sml-to-cortex": SMLToCortexCommand
 };
 
 export async function run() {
